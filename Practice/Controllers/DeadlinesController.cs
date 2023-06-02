74e178c6d4c7d1355654e7f1ee70d131a7563c0d
IgnaxA <ignat.panoff@gmail.com>
2023-06-02 19:18:06 +0500

    Разработка 4

---
+++ Practice/Controllers/DeadlinesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice.Controllers
+{
+    public class DeadlinesController : Controller
+    {
+        public DeadlinesController() 
+        {
+
+        }
+
+        public async Task<ViewResult> DeadlinesList()
+        {
+            return View();
+        }
+    }
+}
